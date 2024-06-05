b5aee8ca9d5696becf6ae94f2707af8f05eb7f03
ghostNotFounded <arsheya.par@gmail.com>
2024-06-05 12:45:25 +0530

    added project list, projecttype, usemouse hook, model and scene file

---
+++ constants.ts
@@ -0,0 +1,10 @@
+import { ProjectType } from "./types";
+
+export const projects: ProjectType[] = [
+  { name: "Surreal Shot" },
+  { name: "Pastel Palette" },
+  { name: "Monochrome Magic" },
+  { name: "Vintage Vista" },
+  { name: "Bokeh Bliss" },
+  { name: "Candid Capture" },
+];
---
+++ types.ts
@@ -0,0 +1,3 @@
+export interface ProjectType {
+  name: string;
+}
