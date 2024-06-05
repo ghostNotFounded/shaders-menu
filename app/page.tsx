ef6e713096f6c80a298b46e9b99f43432d6e765b
ghostNotFounded <arsheya.par@gmail.com>
2024-06-05 12:56:33 +0530

    added lenis smooth scroll, updated project page

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,28 @@
-import React from "react";
+"use client";
+
+import React, { useEffect } from "react";
+
+import Lenis from "lenis";
 
 import Projects from "@/components/Projects";
 
 export default function Page() {
+  useEffect(() => {
+    const lenis = new Lenis();
+
+    function raf(time: number) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+  }, []);
+
   return (
-    <>
+    <main>
       <div className="h-[50vh]" />
       <Projects />
       <div className="h-[50vh]" />
-    </>
+    </main>
   );
 }
--- components/Projects.tsx
+++ components/Projects.tsx
@@ -4,12 +4,20 @@ import { projects } from "@/constants";
 
 export default function Projects() {
   return (
-    <div>
-      {projects.map((project, idx) => (
-        <div className="text-3xl p-5 cursor-pointer hover:italic" key={idx}>
-          {project.name}
-        </div>
-      ))}
+    <div className="relative mix-blend-difference z-10 text-white h-screen w-full">
+      <ul className="border-b border-neutral-500">
+        {projects.map((project, idx) => (
+          <li
+            className="group text-4xl border-t border-neutral-500 p-5 cursor-pointer flex justify-between"
+            key={idx}
+          >
+            <span className="group-hover:italic">{project.name}</span>
+            <span className="opacity-0 group-hover:opacity-100 text-purple-300">
+              {project.japaneseName}
+            </span>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
